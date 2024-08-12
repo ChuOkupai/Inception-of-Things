5e7844a761825458f35670583943b98e62fcc002
Adrien Soursou <asoursou@student.42.fr>
2024-08-12 15:09:20 +0200

    Fix permissions, Removed hardcoded login

--- p1/scripts/setup_worker.sh
+++ p1/scripts/setup_worker.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-echo "Installing K3s in controller agent mode..."
+wget -qO - https://get.k3s.io | K3S_URL="https://$2:6443" K3S_TOKEN_FILE="$1" sh -s - --flannel-iface eth1
+
+echo "alias k='kubectl'" >> /etc/profile.d/aliases.sh
