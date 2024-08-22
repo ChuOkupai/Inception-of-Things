15fa393e2183fb17677385fe21fd3f2bc29f4d86
Adrien Soursou <asoursou@student.42.fr>
2024-08-22 19:45:31 +0200

    Added basic Hello World app

---
+++ p2/TODO.md
@@ -0,0 +1,3 @@
+# TODO
+
+- [ ] Use an environment variable to deploy app1, app2 and app3
--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 wget -qO - https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --flannel-iface eth1
+sleep 20 # TODO: Fix openapi error
 
 echo "alias k='kubectl'" >> /etc/profile.d/aliases.sh
+
+kubectl apply -f /vagrant/confs
